41aff4143994edff02659ac2859a3b6df2e68a42
Vic <1149833549@qq.com>
2021-08-23 14:44:42 +0800

    方法三 倒转

--- 学习/初级算法/数组/旋转数组/main.cpp
+++ 学习/初级算法/数组/旋转数组/main.cpp
@@ -4,18 +4,17 @@ using namespace std;
 
 class Solution {
 public:
+    void reverse(vector<int>& nums,int start,int end){
+        while(start<end){
+            swap(nums[start],nums[end]);
+            start++;end--;
+        }
+    }
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        int count=__gcd(n,k);
-        for(int start=0;start<count;start++){
-            int current=start;
-            int prev=nums[start];
-            do{
-                int next=(current+k)%n;
-                swap(nums[next],prev);
-                current=next;
-            }while(current!=start);
-        }
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
     }
 };
