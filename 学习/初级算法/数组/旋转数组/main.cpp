2a10fa58c5bfc2846374644741895b2dd8b14684
Vic <1149833549@qq.com>
2021-08-23 12:55:13 +0800

    方法一 额外数组

--- 学习/初级算法/数组/旋转数组/main.cpp
+++ 学习/初级算法/数组/旋转数组/main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int len=nums.size();
-        vector<int>n(len);
-        for(int i=0;i<len;i++){
-            n[(i+k)%len]=nums[i];
+        int length=nums.size();
+        vector<int>n(length);
+        for(int i=0;i<length;i++){
+            n[(i+k)%length]=nums[i];
         }
         nums.assign(n.begin(),n.end());
     }
