d7e070772253c9159948e6782173aaacccf8e1ea
Vic <1149833549@qq.com>
2021-08-25 08:56:38 +0800

    done mine 字典

---
+++ 学习/初级算法/数组/存在重复元素/main.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int len=nums.size();
+        unordered_map<int,bool>map;
+        for(int i=0;i<len;i++){
+            if(map.find(nums[i])==map.end()) map[nums[i]]=true;
+            else return true;
+        }
+        return false;
+    }
+};
